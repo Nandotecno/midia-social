cc4a1caf12eb9c34a2bccdc9cb038bda68599a49
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2022-05-30 15:25:16 -0300

    Add files via upload

---
+++ julia-de-oliveira-hissnauer.php
@@ -0,0 +1,22 @@
+<div class="cab" style="background: url('../assets/img/headers/sobre-bg.jpg') no-repeat;">
+    <div class="text-center">
+        <h2>Nossa Equipe</h2>
+    </div>
+</div>
+
+<section class="inner-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h2>Julia de Oliveira Hissnauer</h2>
+                <hr>                
+                <ul>
+                    <li>Formada em Direito na Universidade Católica de Santos (UNISANTOS) - 2018</li>
+                    <li>Inglês</li>
+            </div>
+            <div class="col-md-4">
+                <img src="assets/img/team/julia-de-oliveira-hissnauer.jpg" alt="Julia de Oliveira Hissnauer" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>

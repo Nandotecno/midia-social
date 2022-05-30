4f452e5898205dbd06f06c6d36aefedec4c2203d
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2022-05-30 17:56:28 -0300

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,227 @@
+<section id="hero">
+  <div class="hero-container" data-aos="fade-up" data-aos-delay="150">
+    <img src="../assets/img/en-logo-capa.png" class="logo-capa" alt="Logotipo Sá e Ferreira">
+   <div class = "fra"> <h2> “We are a specialized board specialized in all areas of law with
+    personalized service since 1991”. </h2> </div>
+    <div class="d-flex">
+      <a href="#about" class="btn-get-started scrollto">Meet the office</a>
+    </div>
+  </div>
+</section>
+<main id="main">
+
+  <section id="about" class="about">
+    <div class="container" data-aos="fade-up">
+      <div class="row">
+
+        <div class="col-lg-6 video-box align-self-baseline" data-aos="zoom-in" data-aos-delay="100">
+          <img src="../assets/img/sobre.jpg" class="img-fluid" alt="">
+
+        </div>
+
+        <div class="col-lg-6 pt-3 pt-lg-0 content">
+          <h3>Who we are</h3>
+          <p>Established in 1991, through the partner ALEXANDRE FERREIRA, the SÁ E FERREIRA ADVOGADOS ASSOCIADOS office has a solid structure, providing clients with safe, innovative and agile legal solutions based on technical excellence, professional expertise and modern administrative management and efficient.
+          </p>
+
+          <a href="sobre" class=" btn btn-dark">Read more</a>
+        </div>
+
+      </div>
+      
+    </div>
+  </section>
+
+  <?php include_once 'areas-atuacao.php'; ?>
+
+  <section id="team" class="team section-bg">
+    <div class="container" data-aos="fade-up">
+
+      <div class="section-title">
+        <h2>Meet each member</h2>
+        <p>Our team</p>
+      </div>
+
+      <div class="row justify-content-center">
+
+        <div class="col-lg-4 col-md-6">
+          <div class="member" data-aos="fade-up" data-aos-delay="100">
+            <a href="alexandre-ferreira">
+              <div class="pic"><img src="../assets/img/team/dr-alexandre-ferreira-e-sa.jpg" class="img-fluid" alt=""></div>
+            </a>
+            <div class="member-info">
+              <h4>Alexandre Ferreira</h4>
+              <span>Partner</span>
+              <div class="social">
+                
+                <a href="https://www.facebook.com/saeferreiraadvogados/"><i class="icofont-facebook"></i></a>
+                <a href="https://www.instagram.com/saeferreiraadvogados/"><i class="icofont-instagram"></i></a>
+                <a href="https://www.linkedin.com/company/saeferreiraadvogados"><i class="icofont-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+          <div class="member">
+            <a href="veridiana-de-sa-e-ferreira">
+              <div class="pic"><img src="../assets/img/team/dra-veridiana-ferreira-e-sa.jpg" class="img-fluid" alt=""></div>
+            </a>
+            <div class="member-info">
+              <h4>Veridiana Machado de Sá e Ferreira</h4>
+              <span>Partner</span>
+              <div class="social">
+                
+                <a href="https://www.facebook.com/saeferreiraadvogados/"><i class="icofont-facebook"></i></a>
+                <a href="https://www.instagram.com/saeferreiraadvogados/"><i class="icofont-instagram"></i></a>
+                <a href="https://www.linkedin.com/company/saeferreiraadvogados"><i class="icofont-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+          <div class="member">
+            <a href="flavio-de-mello-almada-ferreira">
+              <div class="pic"><img src="../assets/img/team/dr-flavio-ferreira-e-sa.jpg" class="img-fluid" alt=""></div>
+            </a>
+            <div class="member-info">
+              <h4>Flávio de Mello Almada Ferreira</h4>
+              <span>Partner</span>
+              <div class="social">
+                
+                <a href="https://www.facebook.com/saeferreiraadvogados/"><i class="icofont-facebook"></i></a>
+                <a href="https://www.instagram.com/saeferreiraadvogados/"><i class="icofont-instagram"></i></a>
+                <a href="https://www.linkedin.com/company/saeferreiraadvogados"><i class="icofont-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+<!--
+       <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+          <div class="member">
+            <a href="julia-de-oliveira-hissnauer">
+              <div class="pic"><img src="../assets/img/team/julia-de-oliveira-hissnauer.jpg" class="img-fluid" alt=""></div>
+            </a>
+            <div class="member-info">
+              <h4>Julia de Oliveira Hissnauer</h4>
+              <span>Attorney</span>
+              <div class="social">                
+                <a href="https://www.facebook.com/saeferreiraadvogados/"><i class="icofont-facebook"></i></a>
+                <a href="https://www.instagram.com/saeferreiraadvogados/"><i class="icofont-instagram"></i></a>
+                <a href="https://www.linkedin.com/company/saeferreiraadvogados"><i class="icofont-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+        </div> -->
+        
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+          <div class="member">
+            <a href="beatriz-dos-santos-simoes">
+              <div class="pic"><img src="../assets/img/team/beatriz-dos-santos.jpg" class="img-fluid" alt="Beatriz dos Santos Simões"></div>
+            </a>
+            <div class="member-info">
+              <h4>Beatriz dos Santos Simões</h4>
+              <span>Attorney</span>
+              <div class="social">
+
+                <a href="https://www.facebook.com/saeferreiraadvogados/"><i class="icofont-facebook"></i></a>
+                <a href="https://www.instagram.com/saeferreiraadvogados/"><i class="icofont-instagram"></i></a>
+                <a href="https://www.linkedin.com/company/saeferreiraadvogados"><i class="icofont-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+  <?php 
+  include_once 'estrutura.php'; 
+  include_once 'noticias.php';   
+  ?>
+  <section id="contact" class="contact">
+    <div class="container" data-aos="fade-up">
+
+      <div class=" section-title">
+        <h2>Doubts, criticisms or suggestions</h2>
+        <p>Contacts</p>
+      </div>
+
+      <div class="row">
+
+        <div class="col-lg-6">
+
+          <div class="row">
+            <div class="col-md-6">
+              <div class="info-box">
+                <i class="bx bx-map"></i>
+                <h3>Our address</h3>
+                <p>Av. Ana Costa, 255 - cj. 71
+                  Gonzaga - 11060-001 - Santos / SP</p>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="info-box">
+                <i class="bx bx-envelope"></i>
+                <h3>eMail</h3>
+                <p>sefadv@saeferreira.adv.br<br></p>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="info-box mt-4">
+                <i class="bx bx-phone-call"></i>
+                <h3>Phones</h3>
+                <p>+55 13 3223 7001</p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="info-box mt-4">
+              <a href="https://wa.me/send?phone=5513974060774" target="_blank"><img src="../assets/img/whats-logo.png" class="img-fluid"></a>
+               
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-lg-6 mt-4 mt-lg-0">
+          <form action="../form/mail-br.php" method="post">
+            <div class="form-row">
+              <div class="col-md-5 form-group">
+                <input type="text" name="name" class="form-control" id="name" placeholder="Your name" data-rule="minlen:4" data-msg="Enter the name" />
+                
+              </div>
+              <div class="col-md-5 form-group">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Your email" data-rule="email" data-msg="Enter a valid email address" />
+               
+              </div>
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Enter the subject" />
+              <input type="hidden" name="ct" value="en" />
+            </div>
+            <div class="form-group">
+              <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Escreva alguma coisa" placeholder="Message"></textarea>
+             
+            </div>
+           
+            <div class="text-center">
+              <button name="btn" value="btn" class="btn btn-success" type="submit">Enviar</button><br>
+              <?php
+              if (isset($_SESSION['msg']))
+                echo $_SESSION['msg'];
+              unset($_SESSION['msg']);
+              ?>
+            </div>
+          </form>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+</main>
